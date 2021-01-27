72babf390767e2398811bb8a334b79fcc27fac91
Zontex <Zontex@users.noreply.github.com>
2021-01-27 14:52:27 +0800

    move to extensions folder

--- extensions/extension_module/MicroPython/relays.py
+++ extensions/extension_module/MicroPython/relays.py
@@ -2,6 +2,11 @@ from machine import Pin, I2C
 import mcp23017
 import time
 
+# IO12 reserved for powering the board, define it
+power = Pin(12, Pin.OUT)
+# activate the board
+power.value(1)
+
 i2c = I2C(scl=Pin(33), sda=Pin(32))
 
 mcp = mcp23017.MCP23017(i2c, 0x20)
@@ -15,20 +20,10 @@ mcp.pin(3, mode=0, value=0)
 # open relays
 print("Turn on relays")
 mcp[0].output(1)
-time.sleep(0.5)
-mcp[1].output(1)
-time.sleep(0.5)
-mcp[2].output(1)
-time.sleep(0.5)
-mcp[3].output(1)
-time.sleep(0.5)
+
+#time.sleep(5)
 
 # close relays
-print("Turn off relays")
-mcp[0].output(0)
-time.sleep(0.5)
-mcp[1].output(0)
-time.sleep(0.5)
-mcp[2].output(0)
-time.sleep(0.5)
-mcp[3].output(0)
+#print("Turn off relays")
+#mcp[0].output(0)
+
